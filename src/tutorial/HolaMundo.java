ad9aa5bb85a7c79f2a1ca7534a914632801a5cd3
usuario <usuario@pc20-004>
2023-09-26 11:01:37 +0200

    HEY

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -3,7 +3,7 @@ package tutorial;
 public class HolaMundo {
 	public static void main (String [] args) {
 		int adios =0;
-		System.out.println("Hola CMEEPS");
+		System.out.println("nO TE DEJO ESCRIBIT POR GUIRI jaaaaaaaaaaaaaaaaa");
 		System.out.println("SUSPENSO");
 		Return adios;
 	}
