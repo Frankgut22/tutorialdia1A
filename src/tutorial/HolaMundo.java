5ff68458eed0d71181870e477e4ae65ab057e4af
usuario <usuario@pc20-004>
2023-09-26 11:01:01 +0200

    HOLA Q ASE

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -2,7 +2,9 @@ package tutorial;
 
 public class HolaMundo {
 	public static void main (String [] args) {
+		int adios =0;
 		System.out.println("Hola CMEEPS");
 		System.out.println("SUSPENSO");
+		Return adios;
 	}
 }
